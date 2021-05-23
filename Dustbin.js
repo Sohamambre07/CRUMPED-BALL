7bc33ff8831b5c5bfdf5bc7da0d1addd6d837547
Sohamambre07 <82446205+Sohamambre07@users.noreply.github.com>
2021-05-23 15:04:44 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,22 @@
+class Dustbin{
+
+    constructor(x,y,width,height){
+
+        this.body=Bodies.rectangle(x,y,width,height,{density:90,friction:15})
+        World.add(world,this.body)
+        
+        this.width=width;
+        this.height=height;
+        }
+        
+        
+        display(){
+        
+            push ()
+            fill("white")
+            rectMode(CENTER)
+            rect(this.body.position.x,this.body.position.y,this.width,this.height)
+            pop ()
+        }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CRUMPED-BALL
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
